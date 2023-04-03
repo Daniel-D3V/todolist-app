b281930adc27cb901f6e5835d20b43314012effb
Daniel-Developement <contatodanielsilvaoficial@gmail.com>
2023-04-03 02:38:33 -0300

    Fix save storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,23 @@ import Container from '@/components/Container'
 import Footer from '@/components/Footer';
 import Header from '@/components/Header'
 import Tasks from '@/components/Tasks'
-import { addTask, deleteTaskById, toggleTaskCompletedById } from '@/services/Tasks';
+import { addTask, deleteTaskById, loadTasksFromStorage, saveTasksToStorage, toggleTaskCompletedById } from '@/services/Tasks';
 import { Task } from '@/types/Task';
 import { useEffect, useState } from 'react';
 import styles from './page.module.sass'
 
 export default function RootPage() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const tasks = await loadTasksFromStorage();
+      setTasks(tasks);
+      setIsLoading(false)
+    }
+    fetchTasks();
+  }, [])
 
   const handleCreateTask = (taskTitle: string) => {
     if (taskTitle.length < 1) return;
@@ -25,11 +35,15 @@ export default function RootPage() {
     setTasks(toggleTaskCompletedById(tasks, taskId))
   }
 
+  useEffect(() => {
+    saveTasksToStorage(tasks);
+  }, [tasks])
+
   return (
     <main className={styles.main}>
       <Container>
         <Header handleAddTask={handleCreateTask} />
-        <Tasks tasks={tasks} onDelete={handleDeleteTask} onComplete={handleCompleteTask} />
+        <Tasks isLoading={isLoading} tasks={tasks} onDelete={handleDeleteTask} onComplete={handleCompleteTask} />
         <Footer />
       </Container>
     </main>
--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,14 +1,16 @@
 import { Task } from '@/types/Task'
 import TasksCard from './Card'
 import styles from './styles.module.sass'
+import classnames from 'classnames';
 
 type Props = {
+    isLoading: boolean,
     tasks: Task[],
     onDelete: (taskId: string) => void,
     onComplete: (taskId: string) => void
 }
 
-export default function Tasks({ tasks, onDelete, onComplete }: Props) {
+export default function Tasks({ isLoading, tasks, onDelete, onComplete }: Props) {
     return (
         <div className={styles.tasks}>
             <div className={styles.tasks_header}>
@@ -23,7 +25,14 @@ export default function Tasks({ tasks, onDelete, onComplete }: Props) {
             </div>
 
             <div className={styles.tasks_list}>
-                {tasks.map(task => (
+                {isLoading && (
+                    <div className={styles.loading}>
+                        <span className={classnames(styles.spinner, 'spinner-border')}></span>
+                        <p className={styles.title}>Carregando...</p>
+                    </div>
+                )}
+
+                {!isLoading && tasks.map(task => (
                     <TasksCard task={task} onDelete={onDelete} onComplete={onComplete} key={task.id} />
                 ))}
             </div>
